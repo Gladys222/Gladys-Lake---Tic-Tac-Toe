2537fd8cd5291e978c20038945d4279abdb3645c
Gladysrobinson <gladysrob222@gmail.com>
2026-04-20 11:13:15 -0400

    GetcurrentPlayer(Board board)

--- GladysTicTacToe/src/tictactoe/GameLogic.java
+++ GladysTicTacToe/src/tictactoe/GameLogic.java
@@ -77,8 +77,38 @@ public class GameLogic
 	 public boolean isGameOver(Board board)
 	 {
 	    return checkWin(board, 'X') || checkWin(board, 'O') || isDraw(board);
-	 }        
+	 }     
+	 
+	 public char getCurrentPlayer(Board board)
+	 {
+	     char[][] grid = board.getGrid();
+	     int xCount = 0;
+	     int oCount = 0;
 
+	     for(int row = 0; row < 3; row++)
+	     {
+	         for(int col = 0; col < 3; col++)
+	         {
+	             if(grid[row][col] == 'X')
+	             {
+	                 xCount++;
+	             }
+	             else if(grid[row][col] == 'O')
+	             {
+	                 oCount++;
+	             }
+	         }
+	     }
+
+	     if(xCount == oCount)
+	     {
+	         return 'X';
+	     }
+	     else
+	     {
+	         return 'O';
+	     }
+	 }
 }
 
 
